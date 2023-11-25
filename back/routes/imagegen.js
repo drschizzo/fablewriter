3666a8cb23c8eb368e80b5569250a57e44847967
Henri C <henri.casselle@gmail.com>
2023-11-25 21:13:39 +0100

    gen url saved in file to avoid regenerate images

--- back/routes/imagegen.js
+++ back/routes/imagegen.js
@@ -5,6 +5,8 @@ require('dotenv').config()
 
 var process = require('process');
 var os=require('os');
+var fs = require('fs');
+
 var router = express.Router();
 
 console.log(process.env.OPENAI_API_KEY);
@@ -13,28 +15,49 @@ const openai = new openailib({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
-/* GET users listing. */
 router.get('/', async function(req, res) {
     const prompt = req.query.prompt;
     const demo = req.query.demo;
-    var image_url;
-    if (demo) {
-        image_url='';
+
+    // check if prompt is already in file formatted as "prompt : image_url"
+    // if so, return the corresponding image url
+    var data = fs.readFileSync('gendb', 'utf8');
+    var lines = data.split("\n");
+    var image_url = "";
+    for (var i=0; i<lines.length; i++) {
+        var line = lines[i];
+        var colon_index = line.indexOf(":");
+        var prompt_in_file = line.substring(0, colon_index);
+        if (prompt_in_file == prompt) {
+            image_url = line.substring(colon_index+1);
+            res.setHeader('Content-Type', 'application/json');
+            res.send(image_url);
+            res.end();
+
+            return;
+        }
     }
-    else {
-        
-        const image = await openai.images.generate({
-            model: "dall-e-3",
-            prompt: prompt,
-            n: 1,
-            size: "256x256",
-          });
-          image_url = image.data;
-    }
-    res.setHeader('Content-Type', 'application/json');
+    
+    
+    console.log("prompt : " + prompt);
+    const image = await openai.images.generate({
+        model: "dall-e-3",
+        prompt: prompt + '. dans le style d\'une illustration de livre pour enfants.This scene is reminiscent of a children\'s book illustration, filled with whimsy and innocent charm.',
+        n: 1,
+        size: "1024x1024",
+    });
+    image_url = image.data[0].url;
+
+    // append url and prompt to file
+    fs.appendFile("gendb", prompt + ":" + image_url+ "\n", function(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+
+    res.setHeader('Content-Type', 'text/plain');
     res.send(image_url);
     res.end();
-      
 });
 
 module.exports = router;
