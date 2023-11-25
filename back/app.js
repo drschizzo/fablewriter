cfb263f16ce31a86a75ba98dafefb2f6ab901031
Henri C <henri.casselle@gmail.com>
2023-11-25 18:05:30 +0100

    CORS problem fixed

--- back/app.js
+++ back/app.js
@@ -1,12 +1,23 @@
+var cors = require('cors');
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const corsOptions = {
+    origin: 'http://localhost:4200', // Autoriser les requêtes depuis l'origine de l'application Angular
+    optionsSuccessStatus: 200 // certains navigateurs legacy (IE11, divers SmartTVs) chokent sur 204
+  };
+  
+
 var indexRouter = require('./routes/index');
 var storyRouter = require('./routes/storygen');
 
 var app = express();
+
+  // Activation de CORS pour toutes les requêtes
+  app.use(cors(corsOptions));
 
 app.use(logger('dev'));
 app.use(express.json());
