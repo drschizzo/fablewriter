0b1a60941344f94439616cfc0d81abcabf86d18b
Henri C <henri.casselle@gmail.com>
2023-11-26 11:42:49 +0100

    generation promp updated

--- back/routes/storygen.js
+++ back/routes/storygen.js
@@ -18,7 +18,10 @@ router.get('/', async function(req, res) {
     const prompt = req.query.prompt;
     const demo = req.query.demo;
     var jsonresp;
-    if (demo) {
+    console.log("prompt : " + prompt);
+    console.log("demo : " + demo);
+    if (demo==='true') {
+        console.log("demo mode");
         jsonresp=fakestory;
     }
     else {
@@ -29,26 +32,34 @@ router.get('/', async function(req, res) {
                     content: `Tu es un bot qui raconte des histoires. 
                     L'utilisateur va te passer une serie d'information 
                     et à partir de cela tu dois generer une histoire 
-                    composée d'une vingtaine de phrases. 
+                    composée d'une quinzaine de phrases. 
                     Chaque phrase doit etre associé à une description 
                     d'une illustration qui pourrait illustrer la phrase.
+                    La description doit etre tres précise et doit
+                    permettre à un illustrateur de dessiner l'illustration
+                    sans avoir besoin de plus d'information et s assurer une
+                    cohérence de style et de représentation entre les illustrations.
+                    Chaque description dont contenir des informations precises sur le
+                    style graphique de l'illustration et d'un nom d'artiste qui
+                    pourrait illustrer cette phrase. L'artiste doit etre le meme 
+                    pour toutes les phrases de l'histoire et doit etre repeté dans chaque description.
                     Tu continues à generer des phrases jusqu'à ce que
-                    tu aies atteint la limite de 20 phrases.
-                    Tu formateras ton histoire en JSON de la maniere suivante:
-                    [
-                    {   text: "phrase1",
-                        desc: "description1" },
-                        {   text: "phrase2",
-                            desc: "description2" },
-                    ...
-                    ]
+                    tu aies atteint la limite de 15 phrases.
+                    Tu formateras ton histoire en JSON ayant exactement la forme suivante:
+                    {
+                        story:[
+                        {text: "phrase1",desc: "description1" },
+                        {text: "phrase2",desc: "description2" },
+                        ...
+                        ]
+                    }
                     `,
                 },
                 { role: "user", content: prompt },
             ],
         // model: "gpt-3.5-turbo-1106",
         model:"gpt-4-1106-preview",
-        // response_format: { type: "json_object" },
+        response_format: { type: "json_object" },
             //max_tokens: 4096,
         });
         jsonresp=completion.choices[0].message.content;
